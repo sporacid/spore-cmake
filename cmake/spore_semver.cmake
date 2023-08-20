29039a57b06e43cb58a1d8cb0eee50a5ef9fe10e
sporacid <sporacid@gmail.com>
2023-08-20 15:33:55 -0400

    fix: Check whether .git path exists instead of invoking git

--- cmake/spore_semver.cmake
+++ cmake/spore_semver.cmake
@@ -3,53 +3,49 @@ function(spore_semver SPORE_VERSION_OUT)
   set(SPORE_VERSION_MINOR 0)
   set(SPORE_VERSION_PATCH 0)
 
-  find_package(Git)
 
-  if (GIT_FOUND)
-    execute_process(
-      COMMAND ${GIT_EXECUTABLE} rev-parse --is-inside-work-tree
-      WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
-      OUTPUT_VARIABLE SPORE_GIT_REPOSITORY
-    )
+  if (EXISTS ${CMAKE_CURRENT_SOURCE_DIR}/.git)
+    find_package(Git)
 
-    if (NOT ${SPORE_GIT_REPOSITORY})
-      message(STATUS "Skipping semantic versioning, not in a git repository")
-      return()
-    endif ()
-
-    execute_process(
-      COMMAND ${GIT_EXECUTABLE} --no-pager log --format=%H --reverse
-      WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
-      OUTPUT_VARIABLE SPORE_GIT_COMMITS
-    )
-
-    string(
-      REGEX REPLACE
-      "[\r\n\t ]+"
-      ";"
-      SPORE_GIT_COMMITS
-      ${SPORE_GIT_COMMITS}
-    )
-
-    foreach (SPORE_GIT_COMMIT ${SPORE_GIT_COMMITS})
+    if (GIT_FOUND)
       execute_process(
-        COMMAND ${GIT_EXECUTABLE} --no-pager log -n1 --format=%B ${SPORE_GIT_COMMIT}
+        COMMAND ${GIT_EXECUTABLE} --no-pager log --format=%H --reverse
         WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
-        OUTPUT_VARIABLE SPORE_GIT_COMMIT_BODY
-        OUTPUT_STRIP_TRAILING_WHITESPACE
+        OUTPUT_VARIABLE SPORE_GIT_COMMITS
       )
 
-      if (${SPORE_GIT_COMMIT_BODY} MATCHES ".*BREAKING CHANGES:.*$" OR ${SPORE_GIT_COMMIT_BODY} MATCHES "^[a-zA-Z0-9-_]+(\([a-zA-Z0-9-_]+\))?!:")
-        math(EXPR SPORE_VERSION_MAJOR "${SPORE_VERSION_MAJOR}+1")
-        set(SPORE_VERSION_MINOR 0)
-        set(SPORE_VERSION_PATCH 0)
-      elseif (${SPORE_GIT_COMMIT_BODY} MATCHES "^feat(\([a-zA-Z0-9-_]+\))?:")
-        math(EXPR SPORE_VERSION_MINOR "${SPORE_VERSION_MINOR}+1")
-        set(SPORE_VERSION_PATCH 0)
-      elseif (${SPORE_GIT_COMMIT_BODY} MATCHES "^fix(\([a-zA-Z0-9-_]+\))?:")
-        math(EXPR SPORE_VERSION_PATCH "${SPORE_VERSION_PATCH}+1")
-      endif ()
-    endforeach ()
+      string(
+        REGEX REPLACE
+        "[\r\n\t ]+"
+        ";"
+        SPORE_GIT_COMMITS
+        ${SPORE_GIT_COMMITS}
+      )
+
+      foreach (SPORE_GIT_COMMIT ${SPORE_GIT_COMMITS})
+        execute_process(
+          COMMAND ${GIT_EXECUTABLE} --no-pager log -n1 --format=%B ${SPORE_GIT_COMMIT}
+          WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
+          OUTPUT_VARIABLE SPORE_GIT_COMMIT_BODY
+          OUTPUT_STRIP_TRAILING_WHITESPACE
+        )
+
+        if (${SPORE_GIT_COMMIT_BODY} MATCHES ".*BREAKING CHANGES:.*$" OR ${SPORE_GIT_COMMIT_BODY} MATCHES "^[a-zA-Z0-9-_]+(\([a-zA-Z0-9-_]+\))?!:")
+          math(EXPR SPORE_VERSION_MAJOR "${SPORE_VERSION_MAJOR}+1")
+          set(SPORE_VERSION_MINOR 0)
+          set(SPORE_VERSION_PATCH 0)
+        elseif (${SPORE_GIT_COMMIT_BODY} MATCHES "^feat(\([a-zA-Z0-9-_]+\))?:")
+          math(EXPR SPORE_VERSION_MINOR "${SPORE_VERSION_MINOR}+1")
+          set(SPORE_VERSION_PATCH 0)
+        elseif (${SPORE_GIT_COMMIT_BODY} MATCHES "^fix(\([a-zA-Z0-9-_]+\))?:")
+          math(EXPR SPORE_VERSION_PATCH "${SPORE_VERSION_PATCH}+1")
+        endif ()
+      endforeach ()
+    else ()
+      message(STATUS "Git executable not found, default semantic version")
+    endif ()
+  else ()
+    message(STATUS "Source directory is not a git repository, default semantic version")
   endif ()
 
   set(${SPORE_VERSION_OUT} "${SPORE_VERSION_MAJOR}.${SPORE_VERSION_MINOR}.${SPORE_VERSION_PATCH}" PARENT_SCOPE)
